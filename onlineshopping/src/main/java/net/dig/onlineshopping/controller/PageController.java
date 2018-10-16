7ee6a6d891cb47a2105b3c619e71c4a70c41727e
Digvijay <digsingh144@gmail.com>
2018-10-16 22:07:50 +0530

    Completed the interface theme

--- onlineshopping/src/main/java/net/dig/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/dig/onlineshopping/controller/PageController.java
@@ -12,25 +12,27 @@ public class PageController {
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to spring mvc");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome","true");
 		return mv;
 	}
 	
-//	@RequestMapping(value= {"/test"})
-//	public ModelAndView test(@RequestParam (value="greeting",required=false)String greeting) {
-//		if(greeting==null)
-//			greeting="Welcome digvijay";
-//		ModelAndView mv=new ModelAndView("page");
-//		mv.addObject("greeting",greeting);
-//		return mv;
-//	}
-	
-	@RequestMapping(value= "/test/{greeting}")
-	public ModelAndView test(@PathVariable ("greeting")String greeting) {
-		if(greeting==null)
-			greeting="Welcome digvijay";
+	@RequestMapping(value= "about")
+	public ModelAndView about() {
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout","true");
 		return mv;
 	}
+	
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact","true");
+		return mv;
+	}
+
+	
+
 }
